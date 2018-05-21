163e465d2a7ba54370144f55d84b2a24ab6ac4f6
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-05-22 00:32:15 +0800

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.db.models import Model, QuerySet, Manager, CharField, DateTimeField
+from django.utils import timezone
+
+
+# Create your models here.
+# These models are for audit trail
+# When an object is deleted, it doesnt get removed it just gets archived
+#refer to
+
+
+class SoftDeletionQuerySet(QuerySet):
+    def delete(self):
+        return super(SoftDeletionQuerySet, self).update(archived_at=timezone.now())
+
+    def undelete(self):
+        return super(SoftDeletionQuerySet, self).update(archived_at=None, user=None)
+
+    def hard_delete(self):
+        return super(SoftDeletionQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(archived_at=None)
+
+    def dead(self):
+        return self.exclude(archived_at=None)
+
+
+class SoftDeletionManager(Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', False)
+        self.archived_only = kwargs.pop('archived_only', False)
+
+        super(SoftDeletionManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+
+        if self.alive_only:
+            return SoftDeletionQuerySet(self.model).filter(archived_at=None)
+
+        if self.archived_only:
+            return SoftDeletionQuerySet(self.model).exclude(archived_at=None)
+
+        return SoftDeletionQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+
+class SoftDeletionModel(Model):
+    archived_at = DateTimeField(blank=True, null=True)
+    objects = SoftDeletionManager()
+    current = SoftDeletionManager(alive_only=True)
+    archived = SoftDeletionManager(archived_only=True)
+    archiver = CharField(max_length=32, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, **kwargs):
+        self.archived_at = timezone.now()
+        self.archiver = kwargs['user']
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeletionModel, self).delete()
+
+    def undelete(self):
+        self.archived_at = None
+        self.archiver = ""
+        self.save()
