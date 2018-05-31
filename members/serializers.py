45a630e3df73f942714e81f2415dd722e7afd6ae
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-05-31 13:52:43 +0800

    Configured Response to be in JSON form

--- members/serializers.py
+++ members/serializers.py
@@ -4,8 +4,17 @@ from .models import *
 
 class DriverSerializer(ModelSerializer):
     class Meta:
+        # these are the settings that come with the class
+        # we specify that this serializes the driver class
+        # and we want all fields to be included
+
         model = Driver
         fields = "__all__"
+
+    # def create(self, validated_data):
+    #     # this overrides the parent class' create function
+    #     # by default, it takes in the model's (in this case Driver) --
+    #     # fields and creates an object
 
 
 class PersonSerializer(ModelSerializer):
--- members/views.py
+++ members/views.py
@@ -4,42 +4,40 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from members.serializers import PersonSerializer
+from members.serializers import PersonSerializer, DriverSerializer
 from .models import *
 import json
-
-
-class PersonHandler:
-    @staticmethod
-    def create_person(data):
-        return Person.objects.create(data)
 
 
 class DriverView(APIView):
     @staticmethod
     def get(request):
+        # transform django objects to JSON (so it can be interpreted in the front-end_
+        drivers = DriverSerializer(Driver.objects.all(), many=True)
+        # returns all driver objects
         return Response(data={
-            'drivers': Driver.objects.all()
+            "drivers": drivers.data
         }, status=200)
 
     @staticmethod
     def post(request):
-        # get the request data in JSON format
-        body = json.loads(request.body)
+        # extracts the body from the request
+        data = json.loads(request.body)
 
-        # transform JSON into python dictionary
-        person_serializer = PersonSerializer(data=body)
+        # transform JSON into python object
+        # please read serializers.py Person and Driver serializer
+        driver_serializer = DriverSerializer(data=data)
 
-        if person_serializer.is_valid():
+        if driver_serializer.is_valid():
             # Serializer class has a built in function that creates
             #  an object attributed to it
             # I pass the validated data and it creates the object
-            person = person_serializer.create(validated_data=
-                                              person_serializer.validated_data)
+            driver = driver_serializer.create(validated_data=
+                                              driver_serializer.validated_data)
             return Response(data={
-                'person': person.name
+                'driver_name': driver.name
             }, status=200)
         else:
             return Response(data={
-                "errors": person_serializer.errors
+                "errors": driver_serializer.errors
             })
