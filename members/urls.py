63afee3506be35e14b81fd0aca301e64125adf95
iramacazo <ira_macazo@dlsu.edu.ph>
2018-06-21 08:31:51 +0800

    Added a URL

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,6 @@ from members.views import *
 
 members_urls = [
     path('drivers/', DriverView.as_view()),
-    path('members/', MemberView.as_view())
+    path('members/', MemberView.as_view()),
+    path('members/<int:pk>', MemberView.as_view())
 ]
