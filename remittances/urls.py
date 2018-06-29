634297dd5f9148a43bfd9d653d3e68036b56096e
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2018-06-29 18:44:39 +0800

    Added AssignedTickets View

--- remittances/urls.py
+++ remittances/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from remittances.views import *
+
 
 remittance_urls = [
-
+    path('assigned_tickets/', AssignedTicketView.as_view())
 ]
--- remittances/views.py
+++ remittances/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from remittances.serializers import *
+from .models import *
+import json
+
+
+class AssignedTicketView(APIView):
+    @staticmethod
+    def get(request):
+        assigned_tickets = AssignedTicketSerializer(AssignedTicket.objects.all(), many=True)
+
+        return Response(data={
+            "assigned_tickets": assigned_tickets.data
+        }, status=status.HTTP_200_OK)
+
+
+    @staticmethod
+    def post(request):
+        data = json.loads(request.body)
+        assigned_tickets_serializer = AssignedTicketSerializer(data=data)
+
+        if assigned_tickets.is_valid():
+            assigned_tickets = assigned_tickets_serializer.create(validated_data=assigned_tickets_serializer.validated_data)
+
+            return Response(data={
+                "assigned_tickets_range_from": assigned_tickets.range_from,
+                "assined_tickets_range_to": assigned_tickets.range_to
+                "assigned_tickets_driver": assigned_tickets.driver
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(data={
+                "errors": assigned_tickets_serializer.errors
+            })
+
+
+    @staticmethod
+    def delete(request, pk):
+        AssignedTicket.objects.get(id=pk).delete(user=request.user.username)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    @staticmethod
+    def put(request):
+        pass
