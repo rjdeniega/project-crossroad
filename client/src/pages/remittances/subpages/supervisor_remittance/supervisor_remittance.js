48c8abbe58967eab2f204c506455c1afefa62133
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-07-04 21:03:00 +0800

    Added Supervisor Remittance View

--- client/src/pages/remittances/subpages/supervisor_remittance/supervisor_remittance.js
+++ client/src/pages/remittances/subpages/supervisor_remittance/supervisor_remittance.js
@@ -12,24 +12,87 @@ import {users} from 'react-icons-kit/feather/'
 import {u1F46E} from 'react-icons-kit/noto_emoji_regular/u1F46E'
 import {driversLicenseO} from 'react-icons-kit/fa/driversLicenseO'
 import {cube} from 'react-icons-kit/fa/cube'
-import {Avatar, List, Tag, Tabs} from 'antd'
+import {Avatar, List, Tag, Tabs, Steps, Button, Input} from 'antd'
 import {userCircleO} from 'react-icons-kit/fa/userCircleO'
 import {UserAvatar} from "../../../../components/avatar/avatar"
 import {money} from 'react-icons-kit/fa/money'
 import './style.css'
+import {Stepper} from '../../components/supervisor_stepper/supervisor_stepper'
 import {TicketingPane} from '../../tabs/ticketing/ticketing'
 import {BeepPane} from '../../tabs/beep/beep'
 import {OverviewPane} from '../../tabs/overview/overview'
 const TabPane = Tabs.TabPane;
+const Step = Steps.Step;
 
-export class SupervisorRemittancePage extends Component {
-    state = {
-        currentTab: <TicketingPane/>,
-    };
-
+export class FirstContent extends Component {
     render() {
         return (
+            <div>
+                <p className="username-label">Please enter username</p>
+                <Input className="username" type="text" placeholder="username"/>
+            </div>
+        );
+    }
+}
+export class SecondContent extends Component {
+    render() {
+        return (
+            <div>
+                <p className="name-label">Please enter name</p>
+                <Input className="name" type="text" placeholder="username"/>
+                <p className="name-label">Please enter address</p>
+                <Input className="name" type="text" placeholder="laguna"/>
+                <p className="name-label">Please enter email</p>
+                <Input addonAfter=".com" placeholder="someone@belair"/>
+                <p className="name-label">Please enter contact number</p>
+                <Input addonBefore="+639" placeholder=""/>
+            </div>
+        );
+    }
+}
+export class LastContent extends Component {
+    render() {
+        return (
+            <div>
+                <p> User's temporary password is <b>imabelairboy</b></p>
+            </div>
+        );
+    }
+}
 
+
+const steps = [{
+    title: 'Start Shift',
+    content: <FirstContent/>,
+}, {
+    title: 'Deploy Drivers',
+    content: <SecondContent/>,
+}, {
+    title: 'Confirm',
+    content: <LastContent/>,
+}];
+
+export class SupervisorRemittancePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 0,
+        };
+    }
+
+    next() {
+        const current = this.state.current + 1;
+        this.setState({current});
+    }
+
+    prev() {
+        const current = this.state.current - 1;
+        this.setState({current});
+    }
+
+    render() {
+        const {current} = this.state;
+        return (
             <div className="remittance-page-body">
                 <div className="supervisor-remittance-header">
                     <div className="header-text">
@@ -37,9 +100,42 @@ export class SupervisorRemittancePage extends Component {
                         <div className="page-title"> Remittances</div>
                         <div className="rem-page-description"> Manage Driver Deployment</div>
                     </div>
+                    <div className="header-steps-wrapper">
+                        <div className="header-steps">
+                            <Steps current={current}>
+                                {steps.map(item => <Step key={item.title} title={item.title}/>)}
+                            </Steps>
+                        </div>
+                    </div>
                     <UserAvatar/>
                 </div>
                 <div className="supervisor-rem-content">
+                    <div className="sv-transactions">
+                        <div className="sv-steps-content">{steps[this.state.current].content}</div>
+                        <div className="steps-action">
+                            {
+                                this.state.current < steps.length - 1
+                                &&
+                                <Button type="primary" onClick={() => this.next()}>Next</Button>
+                            }
+                            {
+                                this.state.current === steps.length - 1
+                                &&
+                                <Button type="primary" onClick={this.props.handleOk}>Done</Button>
+                            }
+                            {
+                                this.state.current > 0
+                                &&
+                                <Button style={{marginLeft: 8}} onClick={() => this.prev()}>
+                                    Previous
+                                </Button>
+                            }
+                        </div>
+                    </div>
+                    <div className="sv-pending-requests">
+                    </div>
+                    <div className="sv-completed-requests">
+                    </div>
                 </div>
             </div>
 
