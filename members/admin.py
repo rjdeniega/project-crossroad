fdf6e5a7b73d293c0f8d0667bffd4375c3ace010
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2018-07-10 16:47:39 +0800

    Added Supervisor Class to admin.py
    - added supervisor to members/admin.py

--- members/admin.py
+++ members/admin.py
@@ -4,6 +4,7 @@ from members.models import *
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Driver)
+admin.site.register(Supervisor)
 admin.site.register(Member)
 admin.site.register(IDCards)
 admin.site.register(Prospect)
