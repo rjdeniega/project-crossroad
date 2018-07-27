290099cf2f573c32aaffaa7145374fcd317b5c78
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-07-28 05:03:52 +0800

    Fixed Maintenance Network Memory Leak
    - added an if statement preventing repeated requests
    - removed constructor that causes state to be undefined

--- client/src/pages/maintenance/components/list_of_shuttles/list_of_shuttles.js
+++ client/src/pages/maintenance/components/list_of_shuttles/list_of_shuttles.js
@@ -1,48 +1,51 @@
-import React, {Component} from 'react'
-import {Icon} from 'react-icons-kit'
-import {ic_airport_shuttle} from 'react-icons-kit/md/ic_airport_shuttle'
-import {AddShuttle} from '../add_shuttle/add_shuttle'
-import {ShuttleCards} from "./shuttle_card/shuttle_card";
+import React, { Component } from 'react'
+import { Icon } from 'react-icons-kit'
+import { ic_airport_shuttle } from 'react-icons-kit/md/ic_airport_shuttle'
+import { AddShuttle } from '../add_shuttle/add_shuttle'
+import { ShuttleCards } from "./shuttle_card/shuttle_card";
 
 
-export class ListOfShuttles extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            shuttles: [],
+export class ListOfShuttles extends Component {
+    state = {
+        shuttles: []
+    };
+
+    componentDidMount() {
+        this.fetchShuttles()
+    }
+
+    componentDidUpdate() {
+        this.fetchShuttles()
+    }
+
+    fetchShuttles() {
+        const { shuttles } = this.state;
+        if (shuttles.length == 0) {
+            fetch('inventory/shuttles/')
+                .then(response => {
+                    return response;
+                })
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({ shuttles: data.shuttles })
+                });
         }
     }
 
-    componentDidMount(){
-        this.fetchShuttles()
-    }
+    render() {
+        const { shuttles } = this.state;
 
-    componentDidUpdate(){
-        this.fetchShuttles()
-    }
-
-    fetchShuttles(){
-        fetch('inventory/shuttles/')
-            .then(response => {
-                return response;
-            })
-            .then(response => response.json())
-            .then(data => this.setState({shuttles: data.shuttles}));
-    }
-
-    render(){
-        const {shuttles} = this.state;
-
-        if (shuttles.length === 0){
-            return(
+        if (shuttles.length === 0) {
+            return (
                 <div align="center">
                     <Icon icon={ic_airport_shuttle} size={150}/>
                     <h2>There are no shuttles</h2>
                     <AddShuttle/>
                 </div>
             )
-        }else{
-            return(
+        }
+        else {
+            return (
                 <div>
                     <ShuttleCards shuttles={shuttles} fetchShuttles={this.fetchShuttles}/>
                 </div>
