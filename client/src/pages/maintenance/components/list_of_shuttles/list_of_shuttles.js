c988ab64c970ca12d9441a8aa85634516c07915a
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-07-28 05:08:19 +0800

    Revert "Fixed Maintenance Network Memory Leak"
    This reverts commit 290099cf2f573c32aaffaa7145374fcd317b5c78.

--- client/src/pages/maintenance/components/list_of_shuttles/list_of_shuttles.js
+++ client/src/pages/maintenance/components/list_of_shuttles/list_of_shuttles.js
@@ -1,51 +1,48 @@
-import React, { Component } from 'react'
-import { Icon } from 'react-icons-kit'
-import { ic_airport_shuttle } from 'react-icons-kit/md/ic_airport_shuttle'
-import { AddShuttle } from '../add_shuttle/add_shuttle'
-import { ShuttleCards } from "./shuttle_card/shuttle_card";
+import React, {Component} from 'react'
+import {Icon} from 'react-icons-kit'
+import {ic_airport_shuttle} from 'react-icons-kit/md/ic_airport_shuttle'
+import {AddShuttle} from '../add_shuttle/add_shuttle'
+import {ShuttleCards} from "./shuttle_card/shuttle_card";
 
 
-export class ListOfShuttles extends Component {
-    state = {
-        shuttles: []
-    };
-
-    componentDidMount() {
-        this.fetchShuttles()
-    }
-
-    componentDidUpdate() {
-        this.fetchShuttles()
-    }
-
-    fetchShuttles() {
-        const { shuttles } = this.state;
-        if (shuttles.length == 0) {
-            fetch('inventory/shuttles/')
-                .then(response => {
-                    return response;
-                })
-                .then(response => response.json())
-                .then(data => {
-                    this.setState({ shuttles: data.shuttles })
-                });
+export class ListOfShuttles extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            shuttles: [],
         }
     }
 
-    render() {
-        const { shuttles } = this.state;
+    componentDidMount(){
+        this.fetchShuttles()
+    }
 
-        if (shuttles.length === 0) {
-            return (
+    componentDidUpdate(){
+        this.fetchShuttles()
+    }
+
+    fetchShuttles(){
+        fetch('inventory/shuttles/')
+            .then(response => {
+                return response;
+            })
+            .then(response => response.json())
+            .then(data => this.setState({shuttles: data.shuttles}));
+    }
+
+    render(){
+        const {shuttles} = this.state;
+
+        if (shuttles.length === 0){
+            return(
                 <div align="center">
                     <Icon icon={ic_airport_shuttle} size={150}/>
                     <h2>There are no shuttles</h2>
                     <AddShuttle/>
                 </div>
             )
-        }
-        else {
-            return (
+        }else{
+            return(
                 <div>
                     <ShuttleCards shuttles={shuttles} fetchShuttles={this.fetchShuttles}/>
                 </div>
