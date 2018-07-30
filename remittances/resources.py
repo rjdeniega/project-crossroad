9b84dead5522fcf141dcf50aa1bdc73060b9f305
rjdeniega <ronald_deniega@dlsu.edu.ph>
2018-07-31 00:57:49 +0800

    Added Layer of Instance Duplication for Beep

--- remittances/resources.py
+++ remittances/resources.py
@@ -7,3 +7,16 @@ class BeepTransactionResource(ModelResource):
         model = BeepTransaction
         fields = ('total', 'card_number', 'id', 'shift')
         exclude = ('archiver', 'archived_at')
+
+    def get_or_init_instance(self, instance_loader, row):
+        """
+        Either fetches an already existing instance or initializes a new one.
+        """
+        instance = self.get_instance(instance_loader, row)
+        if instance:
+            instance.total += row.total
+            instance.save()
+            print(instance)
+            return instance, False
+        else:
+            return self.init_instance(row), True
