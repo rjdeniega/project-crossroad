70c879a269cb32a0fbbc5dbfe6ac7eb7a53e1817
iramacazo <ira_macazo@dlsu.edu.ph>
2018-08-01 17:51:31 +0800

    Completed the repairs view module

--- client/src/pages/maintenance/components/list_of_shuttles/shuttle_card/shuttle_actions/repair_tabs/display_repair.js
+++ client/src/pages/maintenance/components/list_of_shuttles/shuttle_card/shuttle_actions/repair_tabs/display_repair.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react'
+import {Icon} from 'react-icons-kit'
+import {List} from 'antd'
+import {ic_access_time} from 'react-icons-kit/md/ic_access_time'
+import {ic_done} from 'react-icons-kit/md/ic_done'
+import {ic_loop} from 'react-icons-kit/md/ic_loop'
 
 export class RepairDisplay extends Component{
     constructor(props){
@@ -10,37 +15,72 @@ export class RepairDisplay extends Component{
         }
     }
 
-
-    componentDidMount(){
-        let repair = this.props.loadedRepair
-        console.log(repair)
-        if(typeof repair !== 'undefined'){
-            fetch('inventory/shuttles/repairs/specific/' + repair.id)
-                .then(response => {
-                    return response;
-                })
-                .then(response => response.json())
-                .then(data => console.log(data))
+    checkStatus(status){
+        switch(status){
+            case 'NS':
+                return (<span><Icon size={24} icon={ic_access_time}
+                             style={{color: '#E9C46A',
+                                     verticalAlign: 'middle'}}/> Not Started</span>);
+            case 'IP':
+                return (<span><Icon icon={ic_loop} size={24}
+                            style={{color: '#E9C46A',
+                                    verticalAlign: 'middle'}}/> In Progress</span>);
+            default:
+                return (<span><Icon icon={ic_done}
+                             size={24}
+                             style={{ color: '#42933C',
+                                      verticalAlign: 'middle'}}/> Completed</span>);
         }
-
     }
 
     render(){
-        let repair = this.props.loadedRepair
-        if(!repair.id){
-            return(
-                <div style={{border: 'solid', height: 250, width: '100%',
-                            borderColor: '#E8E8E8', borderRadius: 5,
-                            borderWidth: 1}} align='middle'>
+        let {repair, problems, findings, modifications} = this.props;
+
+        return(
+            <div style={{border: 'solid', width: '100%',
+                        borderColor: '#E8E8E8', borderRadius: 5,
+                        borderWidth: 1, padding: 20}} align={!repair.id ? 'middle' : 'left'}>
+                {!repair.id ? (
                     <h2>Please select a repair</h2>
-                </div>
-            )
-        } else {
-            return(
-                <div>
-                    {repair.id}
-                </div>
-            )
-        }
+                ) : (
+                    <div>
+                        <h3>Shuttle {repair.shuttle} - Repair {repair.id}</h3>
+
+                        <p>{this.checkStatus(repair.status)}</p>
+                        <List size='small' header={<div><h3>Problems</h3></div>}
+                              bordered>
+                              {problems.map(function(problem, index){
+                                  return (
+                                      <List.Item>{problem.description}</List.Item>
+                                  )
+                              })}
+                        </List>
+
+                        {findings.length == 0 ? '' :
+                            (
+                                <List size='small' header={<h3>Findings</h3>}
+                                      bordered>
+                                      {findings.map(function(finding, index){
+                                      return (
+                                          <List.Item>{finding.description}</List.Item>
+                                      )})}
+                                </List>
+                        )}
+
+                        {modifications.length == 0 ? '' :
+                            (
+                                <List size='small' header={<h3>Items Used</h3>}
+                                      bordered>
+                                      {modifications.map(function(modification, index){
+                                      return (
+                                          <List.Item>{modification.quantity}" "
+                                              {modification.item_used}</List.Item>
+                                      )})}
+                                </List>
+                        )}
+                    </div>
+                )}
+            </div>
+        )
     }
 }
