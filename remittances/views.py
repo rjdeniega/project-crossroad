ae1ad421824d9e400da8b361a2233d1e8ee80796
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2018-08-01 15:54:59 +0800

    Revised add discrepancy
    - adding discrepancy now doesn't subtracts from the total of the remittance

--- remittances/views.py
+++ remittances/views.py
@@ -540,7 +540,7 @@ class AddDiscrepancy(APIView):
         data = json.loads(request.body)
         rem_form = RemittanceForm.objects.get(id=remittance_form_id)
         rem_form.discrepancy = data['discrepancy']
-        rem_form.total = rem_form.total - rem_form.discrepancy
+        # rem_form.total = rem_form.total - rem_form.discrepancy
         rem_form.save()
         remittance = ReadRemittanceSerializer(rem_form)
         return Response(data={
