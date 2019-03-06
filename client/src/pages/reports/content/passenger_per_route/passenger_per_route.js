2513af621db2458d750cc704cfa75abd4984fa69
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2019-03-06 19:50:39 +0800

    Semi-fixed chart bugs in passenger per route

--- client/src/pages/reports/content/passenger_per_route/passenger_per_route.js
+++ client/src/pages/reports/content/passenger_per_route/passenger_per_route.js
@@ -23,8 +23,6 @@ export class PassengerPerRoute extends React.Component {
 
         let request = { "start_date": "2019-02-25" };
 
-        
-
         // Add data
         postData('passenger_per_route/', request)
             .then(data => {
@@ -32,7 +30,6 @@ export class PassengerPerRoute extends React.Component {
                     chart.data = data.values;
                 }
             });
-
 
         // Create category axis
         let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
@@ -48,32 +45,51 @@ export class PassengerPerRoute extends React.Component {
         series1.dataFields.categoryX = "day";
         series1.name = "Main Road";
         series1.strokeWidth = 3;
-        series1.bullets.push(new am4charts.CircleBullet());
-        series1.tooltipText = "Passenger Count in {name} on {categoryX}:{valueY}";
+        series1.stroke = am4core.color('blue');
         series1.legendSettings.valueText = "{valueY}";
+
+        let circleBullet1 = series1.bullets.push(new am4charts.CircleBullet());
+        circleBullet1.fill = am4core.color('blue');
+        circleBullet1.strokeWidth = 2;
+
+        let labelBullet1 = series1.bullets.push(new am4charts.LabelBullet());
+        labelBullet1.label.text = "{main_road}";
+        labelBullet1.label.dy = -20;
 
         let series2 = chart.series.push(new am4charts.LineSeries());
         series2.dataFields.valueY = "kaliwa";
         series2.dataFields.categoryX = "day";
         series2.name = 'Kaliwa';
         series2.strokeWidth = 3;
-        series2.bullets.push(new am4charts.CircleBullet());
-        series2.tooltipText = "Passenger Count in {name} on {categoryX}:{valueY}";
+        series2.stroke = am4core.color('orange');
         series2.legendSettings.valueText = "{valueY}";
+
+        let circleBullet2 = series2.bullets.push(new am4charts.CircleBullet());
+        circleBullet2.fill = am4core.color('orange');
+        circleBullet2.strokeWidth = 2;
+
+        let labelBullet2 = series2.bullets.push(new am4charts.LabelBullet());
+        labelBullet2.label.text = "{kaliwa}";
+        labelBullet2.label.dy = -20;
 
         let series3 = chart.series.push(new am4charts.LineSeries());
         series3.dataFields.valueY = "kanan";
         series3.dataFields.categoryX = "day";
         series3.name = 'Kanan';
         series3.strokeWidth = 3;
-        series3.bullets.push(new am4charts.CircleBullet());
-        series3.tooltipText = "Passenger Count in {name} on {categoryX}:{valueY}";
+        series3.stroke = am4core.color('green');
         series3.legendSettings.valueText = "{valueY}";
 
-        // Add chart cursor
+        let circleBullet3 = series3.bullets.push(new am4charts.CircleBullet());
+        circleBullet3.fill = am4core.color('green');
+        circleBullet3.strokeWidth = 2;
+
+        let labelBullet3 = series3.bullets.push(new am4charts.LabelBullet());
+        labelBullet3.label.text = "{kanan}";
+        labelBullet3.label.dy = -20;
+
         chart.cursor = new am4charts.XYCursor();
-        chart.cursor.behavior = "panXY";
-        chart.cursor.xAxis = categoryAxis;
+        chart.cursor.behavior = "zoomY";
 
         // Add legend
         chart.legend = new am4charts.Legend();
