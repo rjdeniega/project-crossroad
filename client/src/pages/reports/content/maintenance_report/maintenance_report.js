70bf60e15a0848453da42908b6b1204751b7161d
rjdeniega <ronald_deniega@dlsu.edu.ph>
2019-03-26 01:48:12 +0800

    Fixed Report Format

--- client/src/pages/reports/content/maintenance_report/maintenance_report.js
+++ client/src/pages/reports/content/maintenance_report/maintenance_report.js
@@ -50,10 +50,10 @@ class ComponentToPrint extends React.Component {
                             <th>Acquired</th>
                             <th>Mileage</th>
                             <th colSpan={3}>M. Frequency</th>
+                             <th>Freq. Total</th>
                             <th colSpan={3}>M. Cost</th>
+                             <th>Cost Total</th>
                             <th colSpan={3}>Ave. M. Cost</th>
-                            <th>Frequency Total</th>
-                            <th>Cost Total</th>
                         </tr>
                         <tr>
                             <th></th>
@@ -62,9 +62,11 @@ class ComponentToPrint extends React.Component {
                             <th>MN</th>
                             <th>INT</th>
                             <th>MJ</th>
+                            <th></th>
                             <th>MN</th>
                             <th>INT</th>
                             <th>MJ</th>
+                            <th></th>
                             <th>MN</th>
                             <th>INT</th>
                             <th>MJ</th>
@@ -83,14 +85,14 @@ class ComponentToPrint extends React.Component {
                                         <td>{item.number_of_minor_maintenance}</td>
                                         <td>{item.number_of_intermediate_maintenance}</td>
                                         <td>{item.number_of_major_maintenance}</td>
+                                         <td><b>{item.total_frequency}</b></td>
                                         <td className="monetary">{item.minor_maintenance_cost}</td>
                                         <td className="monetary">{item.intermediate_maintenance_cost}</td>
                                         <td className="monetary">{item.major_maintenance_cost}</td>
+                                         <td className="monetary"><b>{item.total}</b></td>
                                         <td className="monetary">{item.minor_average_cost}</td>
                                         <td className="monetary">{item.intermediate_average_cost}</td>
                                         <td className="monetary">{item.major_average_cost}</td>
-                                        <td className="monetary"><b>{item.frequency_total}</b></td>
-                                        <td className="monetary"><b>{item.total}</b></td>
                                     </tr>
                                 </Fragment>
                             ))}
@@ -101,21 +103,21 @@ class ComponentToPrint extends React.Component {
                                 <td className="total-line"><b>{this.props.data.minor_count}</b></td>
                                 <td className="total-line"><b>{this.props.data.intermediate_count}</b></td>
                                 <td className="total-line"><b>{this.props.data.major_count}</b></td>
+                                <td className="total-line"><b>{this.props.data.grand_total_frequency}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.minor_total_maintenance_cost}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.intermediate_total_maintenance_cost}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.major_total_maintenance_cost}</b></td>
+                                <td className="total-line monetary"><b>{this.props.data.grand_total}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.minor_average_maintenance_cost}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.intermediate_average_maintenance_cost}</b></td>
                                 <td className="total-line monetary">
                                     <b>{this.props.data.major_average_maintenance_cost}</b></td>
-                                <td className="total-line monetary"><b>{this.props.data.grand_total_frequency}</b></td>
 
-                                <td className="total-line monetary"><b>{this.props.data.grand_total}</b></td>
 
                             </tr>
                         </Fragment>
